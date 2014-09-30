f9d8624e3c8d0f24036452200a7c3e9c2908b515
David Sivocha <david@sivocha.com>
2014-09-30 17:04:47 +0100

    Removed Script lines from Index.html, created inbox.js

--- app/scripts/controllers/inbox.js
+++ app/scripts/controllers/inbox.js
@@ -8,7 +8,7 @@
  * Controller of the angularGmailApp
  */
 angular.module('angularGmailApp')
-  .controller('MainCtrl', function ($scope) {
+  .controller('InboxCtrl', function ($scope) {
     $scope.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
