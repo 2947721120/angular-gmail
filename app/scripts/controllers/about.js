dd70589b703f486936f413a37c90fccfc03f7f1d
David Sivocha <david@sivocha.com>
2014-09-30 15:43:46 +0100

    Installed Yo Angular and dependencies as a Framework for the Project

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name angularGmailApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the angularGmailApp
+ */
+angular.module('angularGmailApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
