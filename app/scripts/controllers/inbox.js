cd63a160ef8bc9423d0e8545066bc14de09666f3
David Sivocha <david@sivocha.com>
2014-10-22 15:16:20 +0100

    Started refining the inbox controller to use the InboxFactory instead

--- app/scripts/controllers/inbox.js
+++ app/scripts/controllers/inbox.js
@@ -8,10 +8,7 @@
  * Controller of the angularGmailApp
  */
 angular.module('angularGmailApp')
-  .controller('InboxCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+  .controller('InboxCtrl', function ($scope, InboxFactory) {
+    InboxFactory.getMessages()
+    	.success()
   });
